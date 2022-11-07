1a49e6650bc590a1fb8c3368711ce1600315f55c
arya065 <arya065@g.educaand.es>
2022-11-07 14:14:01 +0100

    чекнуть как импортировать архивы или что-то такое

---
+++ Tema4/src/main/java/com/mycompany/tema4/Ej4B.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.tema4;
+import java.util.*;
+import java.lang.*;
+import org.apache.commons.lang3.RandomStringUtils;//импорт архивов в файл pom
+public class Ej4B {
+    public static void main(String[] args) {
+        
+    }
+    
+}
